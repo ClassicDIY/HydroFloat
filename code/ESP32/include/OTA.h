5264a0bed95e2ce6e079bd7e20031184cb23bcad
Graham Ross <graham.a.ross@gmail.com>
2025-07-26 08:08:55 -0400

    Added copyright

--- code/ESP32/include/OTA.h
+++ code/ESP32/include/OTA.h
@@ -23,9 +23,14 @@ namespace HydroFloat
         </head><body>
             <div>
                 <h2>{n}</h2>
-                <div style='font-size: .6em;'>Firmware config version '{v}'</div>
+                <div style='font-size: .7em;'>Firmware config version '{v}'</div>
                 <hr>
             </div>
+            <p>Copyright (c) 2025. ClassicDIY</br></p>
+            <p>The source code is hosted and available on Github at <a href="https://github.com/classicdiy/HydroFloat" target="_blank">https://github.com/classicdiy/HydroFloat</a>.</br></p>
+            <p>The software is provided on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.</br></p>
+
+            <h3>Firmware Update</h3>
             <form method='POST' action='/doupdate' enctype='multipart/form-data'>
             <input type='file' name='firmware'>
             <input type='submit' value='Update Firmware'>
